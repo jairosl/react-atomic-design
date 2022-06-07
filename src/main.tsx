860012b33ae4bfa8d912b178e3715384b3acceb5
jairosl <jairo.lima@aluno.uepb.edu.br>
2022-06-07 09:43:04 -0300

    feat: import global styles and create configurantion for react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { GlobalStyle } from "./styles/global"
+
+import { Router } from "./router";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <GlobalStyle />
+    <Router />
+  </>
 )
