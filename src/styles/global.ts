3be31363d1819851019d0fc2f91c5f5456b8294e
jairosl <jairo.lima@aluno.uepb.edu.br>
2022-06-07 09:41:37 -0300

    styles: create global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from 'styled-components';
+
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --orange-50: #F1972C;
+
+    --purple-250: #BFBFCC;
+    --purple-500: #787880;
+    --purple-600: #5A5A66;
+    --purple-700: #41414C;
+
+    --red-50: #FAECEB;
+    --red-150: #EB3B35;
+
+    --green-50: #E8F8E8;
+    --green-400: #36B336;
+
+    --white-50: #FCFDFF;
+    --white-100: #F0F2F5;
+    --white-150: #E1E3E5;
+
+    --gray-100: #E1E3E6;
+
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box; ;
+  }
+
+   html { 
+    @media (max-width: 1080px) {
+      font-size: 93.75%; // 15px
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%; // 14px
+    }
+  }
+
+  body { 
+    background: var(--white-100);
+    -webkit-font-smoothing: antialiased;
+  }
+
+`;
