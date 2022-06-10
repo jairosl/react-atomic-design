f219d8a5d1cd180de6dd06f0e502419ccf90e5a3
jairosl <jairo.lima@aluno.uepb.edu.br>
2022-06-10 14:57:54 -0300

    chore: add alias import services

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ const path = require('path');
 // https://vitejs.dev/config/
 export default defineConfig({
    resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, '/src') }],
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, '/src') }
+    ],
   },
   plugins: [react()]
 })
